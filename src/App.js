4d45f80d701a1db731bbc8b1758bbb978b9d6f24
Costin <dumitrescucostin20@gmail.com>
2022-12-31 13:02:01 -0500

    tech test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [averageInterestingNumber, setAverageInterestingNumber] = useState(0);
+
+  const fetchDeals = async () => {
+    const response = await axios.get(
+      'https://costin.pipedrive.com/v1/deals?api_token=8679a6c6b4e2704cf123599b46ec21c78664992d',
+    );
+
+    const deals = response.data.data;
+    let sum = 0;
+    let count = 0;
+
+    deals.forEach((deal) => {
+      sum += deal['INTERESTING NUMBER'];
+      count++;
+    });
+
+    setAverageInterestingNumber(sum / count);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={fetchDeals}>Press Me</button>
+      <div>Average INTERESTING NUMBER: {averageInterestingNumber}</div>
     </div>
   );
 }
